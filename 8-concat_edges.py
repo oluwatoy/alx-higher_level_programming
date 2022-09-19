32e46e3e624a9349d28c0bf7b30d3e6887189593
Bamidele Emmanuel <107483062+oluwatoy@users.noreply.github.com>
2022-09-19 13:42:53 +0100

    Delete 8-concat_edges.py

--- 8-concat_edges.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented program\
-        language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[106:11] + str[:6]
-print(str)
