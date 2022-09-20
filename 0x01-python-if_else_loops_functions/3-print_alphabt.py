217de7d155b4b29c6d8cedcebfd1dc1266a05e52
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-09-20 10:42:22 -0700

    loops in python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for str in range(97,123):
-    if str == 101 or str ==113:
+for str in range(ord('a'), ord('z')+1):
+    if chr(str) == 'e' or chr(str) == 'q':
         continue
     else:
-        print("{:c}".format(chr(str)), end="")
+        print("{:c}".format(str), end="")
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+
+def islower(c):
+    if (ord(c) > 96) and (ord(c) < 123):
+        return True
+    else:
+        return False
+i = input("Enter a string")
+if islower(i):
+    print("{:s}".format(i), "is lower")
+else:
+    print("{:s}".format(i), "is upper")
