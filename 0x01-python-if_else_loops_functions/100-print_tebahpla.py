8bffea411e6b9b163f5d7e3d3cf192701631a5bd
Bamidele Emmanuel <107483062+oluwatoy@users.noreply.github.com>
2022-09-20 21:02:46 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for st in range(ord('z'), ord('a') - 1, -1):
-    print("{:st}".format((st - (ord('a') - ord('A'))) if st % 2 else st), end='')
+for a in range(122, 96, -1):
+    if(a % 2 != 0):
+        a -= 32
+    print("{:s}".format(chr(a)), end="")
