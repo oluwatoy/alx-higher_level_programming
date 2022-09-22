96e0d3c1b229d1ed5f5148c9c54e47ee33e74f77
Bamidele Emmanuel <107483062+oluwatoy@users.noreply.github.com>
2022-09-22 17:23:40 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,15 +9,15 @@ if __name__ == "__main__":
     operator = sys.argv[2]
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    if(operator in "+-*/"):
-        if operator is "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif operator is "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif operator is "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        else:
-            print("{} / {} = {}".format(a, b, div(a, b)))
+    #if(operator in "+-*/"):
+    if operator is "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif operator is "-":
+        print("{} - {} = {}".format(a, b, sub(a, b))
+    elif operator is "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif operator is "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
