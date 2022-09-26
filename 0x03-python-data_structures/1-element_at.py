127f79b050a9b38995ad47f2ff52db55d6b1e545
Bamidele Emmanuel <107483062+oluwatoy@users.noreply.github.com>
2022-09-26 10:01:46 +0100

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-  if idx < 0 or idx > (len(my_list)-1):
-    return None
-  else:
-    return my_list[idx]
+
+def print_reversed_list_integer(my_list=[]):
+    if(my_list):
+        for x in reversed(my_list):
+            print("{:d}".format(x))
