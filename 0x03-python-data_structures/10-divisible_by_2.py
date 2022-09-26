6b6c0f04572ae98ca2889a04f525a37d239c42d1
Bamidele Emmanuel <107483062+oluwatoy@users.noreply.github.com>
2022-09-26 10:18:36 +0100

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-  for i in my_list:
+  new_list = my_list.copy
+  for i in new_list:
     if i % 2 == 0:
       return True
     else:
       return False
+   return new_list
