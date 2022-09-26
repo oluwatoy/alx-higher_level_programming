a8cab3f2006c646efb14b50ab8e89db9a5b32ee3
Bamidele Emmanuel <107483062+oluwatoy@users.noreply.github.com>
2022-09-26 22:51:25 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
 
-    a = tuple_a or (0, 0)
-    b = tuple_b or (0, 0)
-    if(len(a) is 1):
-        a = (tuple_a[0], 0)
-    if(len(b) is 1):
-        b = (tuple_b[0], 0)
-    return((a[0] + b[0], a[1] + b[1]))
+def add_tuple(tuple_a=(), tuple_b=()):
+    lenA = len(tuple_a)
+    lenB = len(tuple_b)
+# check for tuple_a
+    if lenA < 1:
+        tuple_a = (0, 0)
+    elif lenA < 2:
+        tuple_a = (tuple_a[0], 0)
+
+# check for tuple_b
+    if lenB < 1:
+        tuple_b = (0, 0)
+    elif lenB < 2:
+        tuple_b = (tuple_b[0], 0)
+
+    res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return 
