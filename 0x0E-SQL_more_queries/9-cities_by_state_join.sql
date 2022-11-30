6c43fa8c81cde7a61b5414b959c71905274e9951
Bamidele Emmanuel <107483062+oluwatoy@users.noreply.github.com>
2022-11-30 16:43:55 +0100

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,11 @@
--- cities by states
-SELECT c.id, c.name, s.name FROM cities c, states s
-WHERE c.state_id = s.id
-GROUP BY c.id ASC;
+-- lists all the cities in the database hbtn_0d_usa
+-- Each record should display: cities.id - cities.name - states.name
+-- results must be sorted in ascending order by cities.id
+-- can use only one SELECT statement
+-- the database name will be passed as an argument of the mysql command
+
+  SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states
+   WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
