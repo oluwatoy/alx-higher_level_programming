5ce1da0d4224c43836f5862f76beb851c73b7227
Bamidele Emmanuel <107483062+oluwatoy@users.noreply.github.com>
2022-11-30 16:52:48 +0100

    Update 103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,10 @@
--- This list all tv show ratings
-SELECT tg.name, SUM(tr.rate) AS rating FROM tv_show_genres tgs
-JOIN tv_genres tg
-ON tg.id = tgs.genre_id
-JOIN tv_show_ratings tr
-ON tr.show_id = tgs.show_id
-GROUP BY  tg.name DESC ORDER BY RATING DESC;
+-- Prints and joins records from two tables with a matching field
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+    FROM tv_genres
+    LEFT JOIN tv_show_genres
+        ON tv_genres.id = tv_show_genres.genre_id
+    RIGHT JOIN tv_show_ratings
+        ON tv_show_ratings.show_id = tv_show_genres.show_id
+    WHERE tv_genres.name IS NOT NULL
+    GROUP BY tv_genres.id
+    ORDER BY rating DESC;
